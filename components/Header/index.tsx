d4f1c3c893e539686a07f802fe98801876eadb6a
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-12 13:00:52 -0300

    feat: create component structure

---
+++ components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>
+        Next Website
+      </h1>
+    </header>
+  )
+}
