b3beba6fe0b89991285676adb59cdb450f30e3dd
Matheus Marques <matheus_maghost@hotmail.com>
2021-01-12 14:28:05 -0300

    feat: create about page

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Navigation from '@components/Navigation'
+
+export default function About() {
+  return (
+    <>
+      <Navigation />
+      <header>
+        <h2>Hello from about page!</h2>
+      </header>
+    </>
+  )
+}
